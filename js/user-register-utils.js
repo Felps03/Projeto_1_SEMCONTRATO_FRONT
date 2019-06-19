c979964fa78d114ca32bded71ee4c89c9be5ddad
Willian Barreto Froes <willian.froes_BOLS@pampa.compasso>
2019-06-19 13:15:28 -0300

    alterar em andamento

--- js/user-register-utils.js
+++ js/user-register-utils.js
@@ -411,13 +411,8 @@ register.addEventListener('click', function(event) {
             var lastName = document.getElementById('lastname').value;
             var dateOfBirth = document.getElementById('birthdate').value;
 			var email = document.getElementById('email').value;
-<<<<<<< HEAD
 			// var file_photo = new FormData($('#file_photo')[0]);
 			var file_photo = document.getElementById('photo').value;
-=======
-			
-			var file_photo = document.getElementById('photo').files[0];
->>>>>>> 61c0b0ac033f61c5ea9b668fbce7e3d9e82c3314
             var userName = document.getElementById('username').value;  
             var password = document.getElementById('password').value;  
 	}
@@ -431,17 +426,11 @@ register.addEventListener('click', function(event) {
 		file_photo,
         dateOfBirth       
     }
-<<<<<<< HEAD
 
-	console.log(User.file_photo);
-
-	// const URL = 'http://localhost:3000/users/user/';
-=======
 	console.log('USER: ', User);
 	
 	const URL =  "https://semcontrato.herokuapp.com/users/user";
 	//const URL = 'http://localhost:3000/users/user/';
->>>>>>> 61c0b0ac033f61c5ea9b668fbce7e3d9e82c3314
 
     $.ajax({
         type: "POST",
