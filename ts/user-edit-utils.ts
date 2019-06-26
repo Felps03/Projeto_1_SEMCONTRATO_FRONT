95390e87d9439b520268de6b92932a5d8ef4e621
Felipe da Silva dos Santos <felipe.santos_BOLS@pampa.compasso>
2019-06-26 14:31:42 -0300

    ajax sendEmail

--- ts/user-edit-utils.ts
+++ ts/user-edit-utils.ts
@@ -1,27 +1,6 @@
-import { validate, InputWrapper } from './validate/index'
 import { HOST } from './config/index'
-import * as val from './validate-fns'
+import $ from 'jquery'
 
 //TODO obter email do token para importar dados do usuário e preencher os campos
 
-const emailInput = InputWrapper.fromId('email')
 
-validate(emailInput, val.email)
-
-const form: HTMLFormElement = <HTMLFormElement>document.getElementById('login-form')
-form.addEventListener('submit', (event: Event) => {
-    event.preventDefault()
-
-    let formData = new FormData(form)
-
-    fetch(`${HOST}users/authenticate`, {
-        method: 'GET',
-        body: formData
-    })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
-            location.replace("home.html")
-        })
-        .catch(console.log)
-})
