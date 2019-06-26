ee81204ea689b760095d5772750e6c5443f43f14
Israel Terra Duarte <israel.duarte_BOLS@pampa.compasso>
2019-06-26 16:30:55 -0300

    [FIX] fixing js

---
+++ js/dist/user-recovery-pass-utils.js
@@ -0,0 +1,41 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var index_1 = require("./validate/index");
+var index_2 = require("./config/index");
+var val = __importStar(require("./validate-fns"));
+var listCheck_1 = require("./utils/listCheck");
+var codeInput = index_1.InputWrapper.fromId('code');
+var emailInput = index_1.InputWrapper.fromId('email_rec');
+var passwordInput = index_1.InputWrapper.fromId('password_rec');
+var passwordConfirmInput = index_1.InputWrapper.fromId('password_rec_conf');
+var valFns = [
+    index_1.validate(codeInput, val.code),
+    index_1.validate(emailInput, val.email),
+    index_1.validate(passwordInput, val.password),
+    index_1.validate(passwordConfirmInput, val.passwordConfirm, passwordInput),
+];
+var form = document.getElementById('recovery-code-form');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (listCheck_1.noFalse(valFns)) {
+        var formData = new FormData(form);
+        fetch(index_2.HOST + "users/code/verify", {
+            method: 'POST',
+            body: formData
+        })
+            .then(function (res) { return res.json(); })
+            .then(function (data) {
+            console.log(data);
+            location.replace("index.html");
+        })
+            .catch(console.log);
+    }
+});
+//# sourceMappingURL=user-recovery-pass-utils.js.map
---
+++ js/dist/validate/InputWrapper.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var InputWrapper = (function () {
+    function InputWrapper(el) {
+        this.el = el;
+        this.msgDiv = el.nextElementSibling;
+    }
+    InputWrapper.prototype.setValid = function (valid, msg) {
+        this.el.classList.remove('is-valid');
+        this.el.classList.remove('is-invalid');
+        this.el.classList.add(valid ? 'is-valid' : 'is-invalid');
+        this.msgDiv.className = valid
+            ? 'valid-feedback'
+            : 'invalid-feedback';
+        if (msg !== undefined)
+            this.setMsg(msg);
+    };
+    InputWrapper.prototype.setMsg = function (msg) {
+        this.msgDiv.textContent = msg;
+    };
+    Object.defineProperty(InputWrapper.prototype, "value", {
+        get: function () {
+            return this.el.value;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    InputWrapper.fromId = function (id) {
+        return new InputWrapper(document.getElementById(id));
+    };
+    return InputWrapper;
+}());
+exports.InputWrapper = InputWrapper;
+//# sourceMappingURL=InputWrapper.js.map
