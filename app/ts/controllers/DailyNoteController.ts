9daad2dcc6af5419867eef26e2555537ed013d44
Israel Terra Duarte <israel.duarte_BOLS@pampa.compasso>
2019-06-26 17:23:25 -0300

    [FEAT] refactor to ts

---
+++ app/ts/controllers/DailyNoteController.ts
@@ -0,0 +1,26 @@
+class DailyNoteController {
+    
+    private _yesterday : HTMLInputElement;
+    private _today: HTMLInputElement;
+    private _impediment: HTMLInputElement;
+    private _date: HTMLInputElement;
+    
+    constructor() {
+        this._yesterday = <HTMLInputElement>document.querySelector('#yesterday');
+        this._today = <HTMLInputElement>document.querySelector('#today');
+        this._impediment = <HTMLInputElement>document.querySelector('#impediment');
+        this._date = <HTMLInputElement>document.querySelector('#date');
+    }
+
+    adiciona(event: Event) {
+        event.preventDefault();
+
+        const negociacao = new DailyNote(
+            this._yesterday,
+            this._today,
+            this._impediment,
+            new Date(this._date.value)
+        );
+    }
+
+}
