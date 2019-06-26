54b809494b2421d8163f055271eaceadc0222351
Felipe da Silva dos Santos <felipe.santos_BOLS@pampa.compasso>
2019-06-26 16:49:19 -0300

    type

---
+++ app/ts/models/User.ts
@@ -0,0 +1,14 @@
+class User {
+
+    private _name; 
+    private _lastName; 
+    private _userName; 
+    private _email;
+    private _password;
+    private _dateOfBirth;
+
+    constructor(name, lastName, userName, email, password, dateOfBirth) {
+        
+    }
+
+}
