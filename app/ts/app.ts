e2e2b2c97d89a70960fcd226d7f4f8b43bcf6c49
Juliana Gonçalves Sequeira <juliana.sequeira_BOLS@pampa.compasso>
2019-06-28 16:45:46 -0300

    after merge

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,16 +1,9 @@
 import { UserController } from "./controllers/UserController";
 
 const controller = new UserController();
-<<<<<<< HEAD
-
-let teste = document.querySelector('.form');
-if(teste != null){
-    teste.addEventListener('submit', controller.add.bind(controller));
-}
-=======
 let teste = document.querySelector('.form');
 
 if(teste != null){
     teste.addEventListener('submit', controller.add.bind(controller));
 }
->>>>>>> b46c618c0e690e139dac1942e7b15ff9f1431b98
+
