2fb018dd12ef7c7e443510dc66594eceeda75507
Vitor Jacob Braga Zogbi Filho <vitor.zogbi_BOLS@pampa.compasso>
2019-07-03 17:46:49 -0300

    [FEATURE]user-register reload index

---
+++ app/js/user-register.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    if (localStorage.getItem('tkn')) {
+        window.location.href = "index.html";
+    }
+});
---
+++ app/ts/user-register.ts
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+        
+    if(localStorage.getItem('tkn')){
+        window.location.href = "index.html";
+    }
+});
