6c7b9550a779b58eff85dd58dec398a435a3bb25
Marcos Oleiro <oleiro87@gmail.com>
2019-07-09 02:22:25 -0300

    [Feat] List by date feature done modified:   app-daily-note.html modified:   app/js/dailyNote.js modified:   app/ts/dailyNote.ts

--- app/js/dailyNote.js
+++ app/js/dailyNote.js
@@ -1,8 +1,10 @@
 import { DailyNoteController } from "./controllers/DailyNoteController";
+import { DailyNote } from "./models/index";
 let yesterday = document.querySelector('#yesterday');
 let today = document.querySelector('#today');
 let impediment = document.querySelector('#impediment');
 let date = document.querySelector('#date');
+let dailyesResult = document.querySelector("#dayliesResult");
 const controller = new DailyNoteController();
 let cadastrar = document.querySelector("#daily-form");
 if (cadastrar) {
@@ -18,7 +20,19 @@ function listDateDaily(event) {
         result
             .then(result => {
             result.forEach((r) => {
-                console.log(r);
+                const daily = new DailyNote(r.yesterday, r.today, r.impediment, new Date(r.date));
+                if (dailyesResult) {
+                    const body = document.createElement('tr');
+                    body.innerHTML =
+                        `<tr>
+                            <td>${r.owner}</td>
+                            <td>${daily.Date.getUTCDate()}/${daily.Date.getUTCMonth() + 1}/${daily.Date.getUTCFullYear()} ${daily.Date.getUTCHours()}:${daily.Date.getUTCMinutes()}</td>
+                            <td>${daily.Yesterday}</td>
+                            <td>${daily.Today}</td>
+                            <td>${daily.Impediment}</td>
+                        </tr>`;
+                    dailyesResult.append(body);
+                }
             });
         });
     }
--- app/ts/dailyNote.ts
+++ app/ts/dailyNote.ts
@@ -1,10 +1,11 @@
 import { DailyNoteController } from "./controllers/DailyNoteController";
+import { DailyNote } from "./models/index";
 
 let yesterday = document.querySelector('#yesterday');
 let today = document.querySelector('#today');
 let impediment = document.querySelector('#impediment');
 let date = document.querySelector('#date');
-
+let dailyesResult = document.querySelector("#dayliesResult")
 
 const controller = new DailyNoteController();
 
@@ -26,7 +27,23 @@ function listDateDaily(event: Event) {
             .then(result => {
                 // console.log(result);
                 result.forEach((r: any) => {
-                    console.log(r);
+                    // console.log(r.yesterday);
+                    const daily = new DailyNote(r.yesterday, r.today, r.impediment, new Date(r.date));
+                    // console.log(r.Date);
+                    // console.log(
+                    if (dailyesResult) {
+                        const body = document.createElement('tr');
+                        body.innerHTML =
+                            `<tr>
+                            <td>${r.owner}</td>
+                            <td>${daily.Date.getUTCDate()}/${daily.Date.getUTCMonth() + 1}/${daily.Date.getUTCFullYear()} ${daily.Date.getUTCHours()}:${daily.Date.getUTCMinutes()}</td>
+                            <td>${daily.Yesterday}</td>
+                            <td>${daily.Today}</td>
+                            <td>${daily.Impediment}</td>
+                        </tr>`;
+                        dailyesResult.append(body);
+                    }
+                    // );
                 });
             })
         // .then(response => {
