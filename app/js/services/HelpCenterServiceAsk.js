0ed65830d6c0297e6b3b7481519fe2168b3d65e9
Felps03 <felipe.031293@gmail.com>
2019-07-11 10:16:20 -0300

    [FIX] TOKEN list helpAsk

--- app/js/services/HelpCenterServiceAsk.js
+++ app/js/services/HelpCenterServiceAsk.js
@@ -39,6 +39,7 @@ export class HelpCenterAskService {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('tkn')}`
             }
         });
     }
--- app/ts/services/HelpCenterServiceAsk.ts
+++ app/ts/services/HelpCenterServiceAsk.ts
@@ -42,6 +42,7 @@ export class HelpCenterAskService {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('tkn')}`
             }
         });
     }
