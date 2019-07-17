ec27beca708a372c58b66c585e370e3084d28c33
Marcos Goulart de Oliveira <marcos.oliveira_BOLS@pampa.compasso>
2019-07-17 16:30:06 -0300

    [FIX] Expired code alert box has been added, and password characteres have been minimated                                                      modified: app/ts/controller?AuthenticateController.ts                           modified:  app/ts/user-recovery.ts                                              modified:  package-lock.json                                                    modified:  package.json

--- app/js/user-recovery.js
+++ app/js/user-recovery.js
@@ -1,6 +1,4 @@
 import { PasswordRecoveryController } from './controllers/PasswordRecoveryController';
-import { getUser } from './utils/userData';
-let userData = getUser();
 document.addEventListener("DOMContentLoaded", function (event) {
     if (localStorage.getItem('tkn')) {
         window.location.href = "index.html";
