d5a90217c60897d3e5c62a2dba59fb6380d5bff6
Juliana Gonçalves Sequeira <juliana.sequeira_BOLS@pampa.compasso>
2019-07-24 16:59:55 -0300

    page of asks modified:   app/app-help-asks.html modified:   app/js/app-help-asks.js modified:   app/js/config/index.js modified:   app/ts/app-help-asks.ts modified:   app/ts/config/index.ts

--- app/js/app-help-asks.js
+++ app/js/app-help-asks.js
@@ -1,2 +1,14 @@
-const url = new URLSearchParams(location.search);
-const url_ask_id = url.get('id');
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var url, url_ask_id;
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters: [],
+        execute: function () {
+            url = new URLSearchParams(location.search);
+            url_ask_id = url.get('id');
+            if (!localStorage.getItem('tkn'))
+                document.getElementById('user-main').innerHTML = `<a href="index.html" class="menu-item"><h5><strong>Login</strong></h5></a>`;
+        }
+    };
+});
--- app/ts/app-help-asks.ts
+++ app/ts/app-help-asks.ts
@@ -1,3 +1,31 @@
 const url = new URLSearchParams(location.search);
 const url_ask_id = url.get('id');
 
+import { HelpCenterController } from "./controllers/HelpCenterController";
+import { getUser } from "./utils/userData";
+import { Post } from "./models/Post";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (!localStorage.getItem('tkn')) document.getElementById('user-main').innerHTML = `<a href="index.html" class="menu-item"><h5><strong>Login</strong></h5></a>`;
