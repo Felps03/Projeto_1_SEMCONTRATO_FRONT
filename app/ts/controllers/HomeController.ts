f3a5960e084a5ff0650a8da90e2cccdefd173586
Marcos Goulart de Oliveira <marcos.oliveira_BOLS@pampa.compasso>
2019-07-30 13:25:39 -0300

    MERGE

--- app/ts/controllers/HomeController.ts
+++ app/ts/controllers/HomeController.ts
@@ -27,7 +27,7 @@ export class HomeController {
                         userName: result['userName']
                     }
                     return data
-                });
+                })
         }
     }
 
