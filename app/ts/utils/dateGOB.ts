a6a1cc0f6095102999cc8cc51544d6e488b665c5
Gabriel Veiga <gabriel.veiga_BOLS@compasso.com.br>
2019-08-14 16:35:03 -0300

    [FIX] make gob help center display formatted date
    ps: fixed publish displaying month 1 number below on mobile
    
    	modified:   app/js/utils/dateGOB.js
    	modified:   app/js/utils/publish.js
    	modified:   app/js/views/PostsGOBView.js
    	modified:   app/ts/utils/dateGOB.ts
    	modified:   app/ts/utils/publish.ts
    	modified:   app/ts/views/PostsGOBView.ts

--- app/ts/utils/dateGOB.ts
+++ app/ts/utils/dateGOB.ts
@@ -1,5 +1,13 @@
-export function dateGOB(dateG:string){
+export function dateGOB(dateG: string) {
     let date = dateG.split('-')
     let newDate = date.reverse().map(dateFilter => + dateFilter).join('/')
     return newDate
+}
+
+export function reverseDateGOB(dateG: string) {
+    let date = dateG.split('/')
+    const newDay = ('00' + date[0]).slice(-2)
+    const newMonth = ('00' + date[1]).slice(-2)
+    const newYear = ('00' + date[2]).slice(-4)
+    return `${newYear}-${newMonth}-${newDay}`
 }
